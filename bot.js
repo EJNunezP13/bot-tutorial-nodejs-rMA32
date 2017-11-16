5d8cd704079f2b1d0f319909531c21d74e6e4e4c
EJNunezP13 <33699937+EJNunezP13@users.noreply.github.com>
2017-11-16 16:41:03 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,7 +12,7 @@ function respond() {
       botRegexSlut = /^\/slut/; botRegexMason = /^\/mason/;
       botRegexProp = /^\/prop/;botRegexKys = /^\/kys/; botRegexSlam = /^\/slam/; botRegexBrye = /^\/brye/;
       botRegexGian = /^\/gian/; botRegexScrotes = /^\/scrotes/; botRegexShaki = /^\/shaki/;
-      botRegexDaf = /^\/dafuq/; botRegexMA32 = /^\/pending/; botRegexTrade = /^\/trade/; 
+      botRegexDaf = /^\/dafuq/; botRegexMA32 = /^\/pending/; botRegexTrade = /^\/trade/; botRegexTeam = /^\/team/;
       siege1 = 'https://i.groupme.com/350x419.png.adc8c73a6c1547e0a9e04320296329f8'; siege2 = 'https://i.groupme.com/1279x752.jpeg.aa5d0401e0df495bba4b4e09dc5a6bd7'
       siege3 = 'https://i.groupme.com/960x960.png.006e180e05d841c6a2962e844bf1e6fd';
   var teamAb = ["NE","NO","ARI","PHI","CLE","TEN","OAK","DAL","IND","SEA","CIN","PIT","JAC"
@@ -92,7 +92,7 @@ function respond() {
   }
   else if(request.text && botRegexRules.test(request.text)) {
     this.res.writeHead(200);
-    postMessage("https://docs.google.com/document/d/1hSuEG7oplnx4IX6HGsMOjsWb9TCqC4-F1NLjuBz5PCM/edit");
+    postMessage("https://docs.google.com/document/d/1LmcXFUJS3Q5-d_um_8xYgWVSBFn4muA1InVS2QS54x8/edit?usp=sharing");
     this.res.end();
   } 
   else if(request.text && botRegexGTA.test(request.text)) {
@@ -171,6 +171,11 @@ function respond() {
       postMessage(siege2);
     this.res.end();
   }
+  else if(request.text && botRegexTeam.test(request.text)) {
+    this.res.writeHead(200);
+    postMessage("https://docs.google.com/spreadsheets/d/14Wh-AsCB5sW2mNGzygK7NwLyxhB7b-q1Cuj8sPC7XP0/edit?usp=sharing");
+    this.res.end();
+  } 
   
   else {
     console.log("don't care");
